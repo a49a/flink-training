073479e355708c6177c65d663b2d4c01766be373
wang <rsl4@foxmail.com>
2020-01-10 21:10:42 +0800

    add keySelector demo

---
+++ src/main/java/me/train/flink/lib/KeySelectById.java
@@ -0,0 +1,11 @@
+package me.train.flink.lib;
+
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+public class KeySelectById implements KeySelector<Tuple2<Integer, String>, Integer> {
+    @Override
+    public Integer getKey(Tuple2<Integer, String> item) throws Exception {
+        return item.f0;
+    }
+}
